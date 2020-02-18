b16d1296e7be2699446695e0bea6f775f86c63bc
rwttr <rattachai.gov@gmail.com>
2020-02-18 16:59:45 +0700

    init

---
+++ demo.jl
@@ -0,0 +1,10 @@
+#using VMLS
+#using LinearAlgebra
+using Images#, ImageView
+using FileIO
+
+#img = testimage("mandrill")
+img = load("testimage_RGB1.png")
+
+#sizeof(var) : size of variable in byte
+#size(var) : return a tuple = size of matrix
