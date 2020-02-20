8e975a310586d79c2a20b2b21bff52a4eefde0d3
rwttr <rattachai.gov@gmail.com>
2020-02-20 11:44:53 +0700

    update ImageSegmentation

--- Lib_JuliaImage/CH6_ImageMetadata.jl
+++ Lib_JuliaImage/CH6_ImageMetadata.jl
@@ -11,3 +11,25 @@ arraydata(img)
 # change change metadata 
 img.time = "morning"
 
+# set of pixel from ImageMeta is ImageMeta
+
+# copy version
+# make a copy of metadata : similar()
+c = img[1:2, 1:2];
+display("text/plain",arraydata(c))
+
+# view version
+# share metadata : shareproperties()
+v = view(img, 1:2, 1:2);
+display("text/plain",arraydata(v))
+
+# spatialproperties
+# properties corresponding to axis
+A = reshape(1:15, 3, 5)
+
+# example sum along axis
+img = ImageMeta(A, spatialproperties=Set([:maxsum]), 
+                maxsum=[maximum(sum(A,dims=1)), 
+                maximum(sum(A,dims=2))])
+
+imgp = permutedims(img, (2,1))
