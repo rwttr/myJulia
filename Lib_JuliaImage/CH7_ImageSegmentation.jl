5683c49bbf65d72d80d8b7b3bf9932a5525dbd2f
rwttr <rattachai.gov@gmail.com>
2020-02-20 14:02:43 +0700

    update

--- Lib_JuliaImage/CH7_ImageSegmentation.jl
+++ Lib_JuliaImage/CH7_ImageSegmentation.jl
@@ -1,1 +1,9 @@
 using Images, ImageSegmentation
+
+img = load("Lib_JuliaImage/horse.jpg")
+
+# Store seed point in (seed position, label) tuples
+seeds = [(CartesianIndex(126,81),1), 
+         (CartesianIndex(93,255),2), 
+         (CartesianIndex(213,97),3)]
+segments = seeded_region_growing(img, seeds)
